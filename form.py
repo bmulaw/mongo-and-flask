0e509fdec2bdd0bcffe19d5c0b3653422fa49868
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-51.bu.edu>
2021-09-01 22:07:19 -0400

    form is set up

--- form.py
+++ form.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+app = Flask(__name__, template_folder='template')
 
-@app.route('/')
-def main():
-    return 'hello world'
+@app.route('/', methods =["GET", "POST"])
+def form():
+    if request.method == "POST":
+        num = str(request.form.get('num'))
+        name =  (request.form.get('name'))
+        return name + ", " + num
+
+    return render_template('form.html')
+
+
+        
+
 
 if __name__ == '__main__':
     app.run(debug=True)
