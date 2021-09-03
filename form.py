03eec6ff4ec86cd46d6240599815d88102bfe0d6
Bernard Mulaw <bernard@dhcp-wifi-8021x-168-122-8-245.bu.edu>
2021-09-03 02:49:21 -0400

    final goal of challenge

--- form.py
+++ form.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+
 app = Flask(__name__, template_folder='template')
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/movie"
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+
 @app.route('/', methods =["GET", "POST"])
-def form():
+def insert_film():
     if request.method == "POST":
-        num = str(request.form.get('num'))
+        # https://www.codegrepper.com/code-examples/python/flask+submit+button
+        num = (request.form.get('num'))
         name =  (request.form.get('name'))
-        return name + ", " + num
+        db.movie.insert_one(
+            {'name': name, 
+            'rating': num
+            })
+        return render_template('form.html')
 
     return render_template('form.html')
-
-
-        
 
 
 if __name__ == '__main__':
