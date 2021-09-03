16b5cdaada08fa2cb6753fdef5e567b8717a83ff
Bernard Mulaw <bernard@dhcp-wifi-8021x-168-122-8-245.bu.edu>
2021-09-03 02:53:22 -0400

    cminimized code

--- form.py
+++ form.py
@@ -7,21 +7,15 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/movie"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
-
-@app.route('/', methods =["GET", "POST"])
+@app.route('/', methods =["POST"])
 def insert_film():
-    if request.method == "POST":
-        # https://www.codegrepper.com/code-examples/python/flask+submit+button
-        num = (request.form.get('num'))
-        name =  (request.form.get('name'))
-        db.movie.insert_one(
-            {'name': name, 
-            'rating': num
-            })
-        return render_template('form.html')
-
+    num = request.form.get('num')
+    name = request.form.get('name')
+    db.movie.insert_one(
+        {'name': name, 
+        'rating': num
+        })
     return render_template('form.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
